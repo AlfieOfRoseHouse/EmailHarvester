9c4747358c7f933a91284e12f8c3d24b268ddffc
Alfie <161310786+AlfieOfRoseHouse@users.noreply.github.com>
2025-04-18 11:02:26 +0100

    Update Python.py

--- Python.py
+++ Python.py
@@ -6,16 +6,17 @@ from bs4 import BeautifulSoup
 
 def searchEmails(query):
   headers = {'User-Agent': 'Mozilla/5.0'}
-  url = f'https://www.google.com/search?q={query}'
+  url = f'https://html.duckduckgo.com/html/?q={query}'
   response = requests.get(url, headers=headers)
   soup = BeautifulSoup(response.text, 'html.parser')
+  
   links = [a['href'] for a in soup.find_all('a', href=True)]
-
   emails = set()
   for link in links:
     try:
-      page = requests.get(link)
-      found = re.findall(r'[a-zA-Z0-9._%+-]+@[a-zA-z0-0.-]+\.[a-z]{2,}', page.text)
+      page = requests.get(link, headers=headers)
+      found = re.findall(r'[a-zA-Z0-9._%+-]+@[a-zA-z0-9.-]+\.[a-z]{2,}', page.text)
+      emails.update(found)
     except:
       pass
   return emails
